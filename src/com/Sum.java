f550e66bd2f55d330720e3fc3bd5954cdc6e2121
Shalini <shalini18.india@gmail.com>
2021-08-01 14:38:27 +0200

    Sum Program

---
+++ src/com/Sum.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 18;
+        int c = a +b;
+        System.out.println("Sum : "+c);
+    }
+
+}
